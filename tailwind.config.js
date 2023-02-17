fcfe64076a10fedf36160a009eb78fa603c93bd7
nqan1409 <quangtrungnguyen05021973@gmail.com>
2023-02-17 18:16:52 +0700

    Login UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    mode: 'jit',
+    content: ['./views/**/*.hbs'],
+    theme: {
+        extend: {
+            fontFamily: {
+                inter: ['Inter', 'san-serif'],
+            },
+            colors: {
+                primary: '#1577C2',
+            },
+        },
+    },
+    plugins: [],
 };
