6f80bd25dfd79b27ee02b020802cf6ec1f89cb43
nqan1409 <quangtrungnguyen05021973@gmail.com>
2023-03-03 00:41:43 +0700

    Fix Login form UI

--- public/js/validator.js
+++ public/js/validator.js
@@ -1,6 +1,5 @@
 function Validator(formSelector) {
     let formRules = {};
-    let _this = this;
     let formElement = document.querySelector(formSelector);
 
     function getParent(element, selector) {
@@ -12,41 +11,33 @@ function Validator(formSelector) {
         }
     }
 
-    function getPassword(selector) {
+    const getPassword = (selector) => {
         let pass = formElement.querySelector(selector).value;
         return pass;
-    }
+    };
 
     let validatorRules = {
         required(value, message = 'Vui lòng nhập trường này') {
             return value ? undefined : message;
         },
         min(min) {
-            return function (
-                value,
-                message = `Vui lòng nhập ít nhất ${min} ký tự`
-            ) {
-                return value.length >= min ? undefined : message;
-            };
+            return (value, message = `Vui lòng nhập ít nhất ${min} ký tự`) =>
+                value.length >= min ? undefined : message;
         },
         confirmed() {
-            return function (value, message = 'Mật khẩu không đúng') {
-                return value === getPassword('#password') ? undefined : message;
-            };
+            return (value, message = 'Mật khẩu không đúng') =>
+                value === getPassword('#password') ? undefined : message;
         },
     };
 
-    // Chỉ xử lý khi tìm thấy form element trong DOM
     if (formElement) {
-        let inputs = formElement.querySelectorAll('[name][rules]');
+        let inputs = Array.from(formElement.querySelectorAll('[name][rules]'));
 
-        // Lập qua và gán các validate function cho đối tượng formRules
-        for (let input of inputs) {
+        inputs.forEach((input) => {
             let rules = input.getAttribute('rules').split('|');
             for (let rule of rules) {
                 let ruleFunct = validatorRules[rule];
 
-                // Nếu rule là min (có dạng min:4)
                 if (rule.includes(':')) {
                     let ruleMin = rule.split(':');
                     rule = ruleMin[0];
@@ -57,11 +48,6 @@ function Validator(formSelector) {
                     ruleFunct = validatorRules[rule]();
                 }
 
-                // Nếu formRules[input.name] là mảng thì push rule function mới vào
-                // Nếu không thì gán formRules[input.name] bằng một mảng mới có 1 phần tử là ruleFunct
-                // Với formRules là đối tượng chưa các rule function
-                // [input.name] là key
-                // ruleFunct là value tương ứng với key
                 if (Array.isArray(formRules[input.name])) {
                     formRules[input.name].push(ruleFunct);
                 } else {
@@ -69,14 +55,11 @@ function Validator(formSelector) {
                 }
             }
 
-            // Lắng nghe các sự kiện để validate
             input.onblur = handleValidte;
             input.oninput = handleClear;
-        }
+        });
 
-        // Xử ký validation form
         function handleValidte(e) {
-            // e.target.name là name của input tag và cũng là key của đối tượng formRules
             let inputElement = e.target;
             let rulesFunct = formRules[inputElement.name];
             let errorMessage;
@@ -97,10 +80,8 @@ function Validator(formSelector) {
                     }
                 }
             }
-            //  Trả về true nếu không có lỗi
             return !errorMessage;
         }
-        // Xử lý khi nhập input
         function handleClear(e) {
             let inputElement = e.target;
             let formGroup = getParent(inputElement, '.form-group');
